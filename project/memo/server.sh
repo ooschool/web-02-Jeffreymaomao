f60d1bffe272ed021ad4be01ea7d01de714ec741
Chang Mao Yang <jeffrey0613mao@gmail.com>
2023-06-23 21:38:53 +0800

    server

---
+++ project/memo/server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get directory
+script_dir=$(cd "$(dirname "$0")" && pwd)
+
+# change directory
+cd "$script_dir"
+
+# open local host
+open http://localhost:8080
+
+# python server
+python -m http.server 8080
+
+
